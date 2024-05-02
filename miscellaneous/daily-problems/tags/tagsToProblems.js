12d3d81cc472b102238a4affc69ad1e25f78b773
prasadsachchidanand <prasadsachchidanand1729@gmail.com>
2024-05-02 17:59:39 +0800

    updated tags

--- miscellaneous/daily-problems/tags/tagsToProblems.js
+++ miscellaneous/daily-problems/tags/tagsToProblems.js
@@ -3,6 +3,7 @@ const tagsToProblems = {
     'group-theory': ['../2024-05-01'],
     // 'ring-theory': ['../2024-05-02', '../2024-04-19'],
     // 'field-theory': ['../2024-04-30', '../2024-04-15', '../2023-03-25', '../2023-04-25', '../2023-03-18', '../2023-03-11'],
-    // 'number-theory': ['../2023-08-19', '../2023-08-12'],
+    'topology': ['../2024-05-02'],
+    'metric-space': ['../2024-05-02'],
     // Add more tags and associated problems here
 };
